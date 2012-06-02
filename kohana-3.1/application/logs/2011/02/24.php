1b08b998719d45fe0612bdf04c075cf3ad0d6249
DaSourcerer <webmaster@dasourcerer.net>
2012-06-03 00:57:34 +0200

    This needs to be redone ...

--- kohana-3.1/application/logs/2011/02/24.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-02-24 04:13:56 --- ERROR: Http_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana-3.1/index.php ~ SYSPATH/classes/kohana/request.php [ 733 ]
-2011-02-24 04:14:03 --- ERROR: Http_Exception_404 [ 404 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 733 ]
